e2da70ce2857f9dc694b2d35432c63c32d959c7b
Diego Bermudez <godiegosaenz@gmail.com>
2018-01-04 16:43:23 -0500

    cambio de actividad

--- resources/views/proyectos/actividades.blade.php
+++ resources/views/proyectos/actividades.blade.php
@@ -7,7 +7,7 @@
                     <ul class="breadcrumb push-down-0">
                         <li><a href="{{ url('/')}}">Principal</a></li>
                         
-                        <li class="active">Actividades</li>
+                        <li class="active">Actividades2</li>
                     </ul>
                 <!-- END BREADCRUMB -->  
                                                              
